df8a3e9c3e066f8d51ea823fbed956c858079629
avidrutham <divyamathur89@gmail.com>
2019-03-18 16:31:09 +0530

    connect to database

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -5,6 +6,20 @@ const cors = require("cors");
 const PORT = 5000;
 
 const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(process.env.MLAB_URI, {
+    useNewUrlParser: true
+  })
+  .then(
+    () => {
+      console.log("Database is now connected");
+    },
+    err => {
+      console.log("Cannot connect to database + ", err);
+    }
+  );
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
