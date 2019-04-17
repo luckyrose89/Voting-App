6fbc4acfe91e1d29b3b626004e731258f6f3809f
avidrutham <divyamathur89@gmail.com>
2019-04-17 19:26:48 +0530

    complete types and add polls rootquery

--- backend/schema/schema.js
+++ backend/schema/schema.js
@@ -1,18 +1,43 @@
 const graphql = require("graphql");
 const Poll = require("../models/poll");
 
-const { GraphQLObjectType, GraphQLSchema } = graphql;
-
-const QuestionType = new GraphQLObjectType({
-  name: "Question"
-});
+const {
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLString,
+  GraphQLList,
+  GraphQLInt
+} = graphql;
 
 const AnswerType = new GraphQLObjectType({
-  name: "Answer"
+  name: "Answer",
+  fields: () => ({
+    id: { type: GraphQLID },
+    option: { type: GraphQLString },
+    votes: { type: GraphQLInt }
+  })
+});
+
+const QuestionType = new GraphQLObjectType({
+  name: "Question",
+  fields: () => ({
+    id: { type: GraphQLID },
+    question: { type: GraphQLString },
+    answer: { type: GraphQLList(AnswerType) }
+  })
 });
 
 const RootQuery = new GraphQLObjectType({
-  name: "RootQueryType"
+  name: "RootQueryType",
+  fields: {
+    polls: {
+      type: new GraphQLList(QuestionType),
+      resolve(parent, args) {
+        return Poll.find({});
+      }
+    }
+  }
 });
 
 const Mutation = new GraphQLObjectType({
