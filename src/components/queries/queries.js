a6547106966fb4cb55c7b341e401404693586855
avidrutham <divyamathur89@gmail.com>
2019-04-25 23:48:12 +0530

    define poll queries in queries.js

---
+++ src/components/queries/queries.js
@@ -0,0 +1,22 @@
+import { gql } from "apollo-boost";
+
+const getPollsQuery = gql``;
+
+const getPollQuery = gql``;
+
+const addQuestionQuery = gql``;
+
+const addAnswerQuery = gql``;
+
+const deleteQuestionQuery = gql``;
+
+const deleteAnswerQuery = gql``;
+
+export {
+  getPollsQuery,
+  getPollQuery,
+  addQuestionQuery,
+  deleteQuestionQuery,
+  addAnswerQuery,
+  deleteAnswerQuery
+};
