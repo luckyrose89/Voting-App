2ba98cbde6acfd04a4a29492c8f3d3d8bdcb9c88
avidrutham <divyamathur89@gmail.com>
2019-05-14 23:23:28 +0530

    add reference document removal middleware

--- backend/models/poll.js
+++ backend/models/poll.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Option = require("./pollOptions");
 
 const pollSchema = new Schema({
   question: String
+});
+
+pollSchema.pre("remove", function(doc) {
+  Option.remove({ pollId: doc._id }).exec();
 });
 
 module.exports = mongoose.model("Poll", pollSchema);
