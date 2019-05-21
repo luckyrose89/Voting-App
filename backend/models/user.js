17e68fce9ba0bd09b1b74d405128e5e95020b6e5
avidrutham <divyamathur89@gmail.com>
2019-05-21 23:30:20 +0530

    add jsonwebtoken method to user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const jwt = require("jsonwebtoken");
 
 const userSchema = new Schema({
   name: String,
@@ -10,5 +11,21 @@ const userSchema = new Schema({
     }
   }
 });
+
+// Model methods
+userSchema.methods.generateJWT = function() {
+  const today = new Date();
+  const expirationDate = new Date(today);
+  expirationDate.setDate(today.getDate() + 60);
+
+  return jwt.sign(
+    {
+      name: this.name,
+      id: this._id,
+      exp: parseInt(expirationDate.getTime() / 1000, 10)
+    },
+    "secret"
+  );
+};
 
 module.exports = mongoose.model("User", userSchema);
