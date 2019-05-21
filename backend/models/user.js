a932c306dcac9500b07eb03bcdeabe9a23fd091a
avidrutham <divyamathur89@gmail.com>
2019-05-21 22:25:25 +0530

    add twitter id to user model and add passort

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,7 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: String
+  name: String,
+  social: {
+    twitterProvider: {
+      id: String,
+      token: String
+    }
+  }
 });
 
 module.exports = mongoose.model("User", userSchema);
