26a36997d14da99fb3b7f98b04faa3e962e3f99d
avidrutham <divyamathur89@gmail.com>
2019-05-27 23:44:18 +0530

    rewrite twitter passport strategy

--- backend/passport.js
+++ backend/passport.js
@@ -1,23 +1,35 @@
 var passport = require("passport"),
-  TwitterTokenStrategy = require("passport-twitter-token"),
-  User = require("./models/user");
+  TwitterTokenStrategy = require("passport-twitter-token");
 
-module.exports = function() {
-  passport.use(
-    new TwitterTokenStrategy(
-      {
-        consumerKey: "j4fSytOnp6A7PCnPVG1koBcZK",
-        consumerSecret: "IVBU1FmBQTh6FXxLGshwYkNlgIaFbG4uqJi7VyPlbLF6ihMAdg",
-        includeEmail: true
-      },
-      function(token, tokenSecret, profile, done) {
-        User.upsertTwitterUser(token, tokenSecret, profile, function(
-          err,
-          user
-        ) {
-          return done(err, user);
-        });
-      }
-    )
-  );
-};
+// Twitter Strategy
+const TwitterTokenStrategyCallback = (
+  accessToken,
+  refreshToken,
+  profile,
+  done
+) =>
+  done(null, {
+    accessToken,
+    refreshToken,
+    profile
+  });
+
+passport.use(
+  new TwitterTokenStrategy(
+    {
+      consumerKey: "j4fSytOnp6A7PCnPVG1koBcZK",
+      consumerSecret: "IVBU1FmBQTh6FXxLGshwYkNlgIaFbG4uqJi7VyPlbLF6ihMAdg"
+    },
+    TwitterTokenStrategyCallback
+  )
+);
+
+const authenticateTwitter = (req, res) =>
+  new Promise((resolve, reject) => {
+    passport.authenticate("twitter-token", (err, data, info) => {
+      if (err) reject(err);
+      resolve({ data, info });
+    })(req, res);
+  });
+
+module.exports = { authenticateTwitter };
